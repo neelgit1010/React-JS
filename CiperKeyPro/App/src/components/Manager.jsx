2c2f518abf9d9cc7fe4ad83fb05b84a09f42abbb
neelgit1010 <neel.binbrain1010@gmail.com>
2024-10-24 15:03:48 +0530

    desgining with tcss

--- CiperKeyPro/App/src/components/Manager.jsx
+++ CiperKeyPro/App/src/components/Manager.jsx
@@ -1,9 +1,24 @@
 const Manager = () => {
   return (
+    <>
     <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-
-        
     </div>
+    <div className="text-white mycontainer text-center">
+    <span className="text-green-500">&lt;</span>
+        CiperKey
+    <span className="text-green-500">Pro/&gt;</span>
+    <p className="text-sm">Your own Password Managing tool</p>
+    </div>
+
+    <div className="text-white mycontainer text-center gap-8">
+     <input type="text" placeholder="Enter Password" className="border-2 bg-transparent border-green-500 rounded-md px-4 py-2 mt-5 w-full"/>
+     <div className="flex gap-8">
+     <input type="text" placeholder="Enter Password" className="border-2 bg-transparent border-green-500 rounded-md px-4 py-2 mt-5 w-1/2"/>
+     <input type="text" placeholder="Enter Password" className="border-2 bg-transparent border-green-500 rounded-md px-4 py-2 mt-5 w-1/2"/>
+     </div>
+     <button className="text-white bg-green-600 hover:bg-green-700 rounded-md px-4 py-2 mt-5 w-1/2">Add Password</button>
+    </div>
+    </>
   );
 };
 
--- CiperKeyPro/App/src/components/Navbar.jsx
+++ CiperKeyPro/App/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 
 const Navbar = () => {
   return (
-    <nav className=" bg-purple-500 text-white flex justify-between items-center p-5">
-      <div className="text-3xl font-bold">CiperKey Pro</div>
+    <nav className=" bg-slate-800 text-white flex justify-between items-center p-5">
+      <div className="text-2xl font-bold">
+        <span className="text-green-500">&lt;</span>
+        CiperKey
+        <span className="text-green-500">Pro/&gt;</span>
+      </div>
       <ul className="flex gap-10">
-        <a href="/"> <li>Home</li> </a>
-        <a href="/"> <li>ABout</li> </a>
-        <a href="/"> <li>Contact</li> </a>
+        <a href="/" className="hover:font-bold transition-all"> <li>Home</li> </a>
+        <a href="/" className="hover:font-bold transition-all"> <li>About</li> </a>
+        <a href="/" className="hover:font-bold transition-all"> <li>Contact</li> </a>
       </ul>
     </nav>
   )
